4a8cc33c4a4898b8dbfe74c894c1bf8a0f9b43b5
Wesley Barros <wesleybarros950@gmail.com>
2023-03-05 08:46:24 -0300

    added db_url to env vars

--- fastbb/environment.py
+++ fastbb/environment.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = os.getenv('SECRET_KEY')
-ALGORITHM = os.getenv("ALGORITHM")
+SECRET_KEY = os.getenv('SECRET_KEY', 'asdf')
+ALGORITHM = os.getenv('ALGORITHM', 'HS256')
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES', '30'))
+DB_URL = os.getenv('DB_URL', 'sqlite:///./app.db')
